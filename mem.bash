63f4d1b9ff5a132a07534cd13e95120f8da6f858
ssevenseass <k-kuksenko@mail.ru>
2020-12-28 00:30:23 +0300

    add

--- mem.bash
+++ mem.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
  truncate -s 0 report.log
- declare -a array
+ declare -a array=()
 counter=0
  elements=(1 2 3 4 5 6 7 8 9 10)
 a=0
 while true
 do
-counter+=1
+ let counter+=1
 array+=(${elements[@]})
   if [[ $counter == 100000 ]]
  then
   counter=0
  a="${#array[@]}"
-echo "$a" >> report.log
+echo "${#array[@]}" >> report.log
   fi
 done
